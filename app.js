21a3a06c231053ebbb1411a0882d39f2e5e79c9c
Ashish <as7220004@gmail.com>
2021-06-09 12:47:17 +0530

    Google OAuth20, mongoDB schema, DB collection with user profile data, passport

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
+const passport = require('passport');
+const session = require('express-session');
 const path = require('path');
 
 
 //loading config file
 dotenv.config({path: './config/config.env'});
+
+//passport config
+require('./config/passport')(passport);
 
 //connecting db
 connectDB();
@@ -23,8 +28,21 @@ if(process.env.NODE_ENV === 'development'){
 app.engine('.hbs', exphbs({defaultlayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-//router
+//express-session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+//   cookie: { secure: true }
+}));
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//routes
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
 //static folder
 app.use(express.static(path.join(__dirname, 'public')));
